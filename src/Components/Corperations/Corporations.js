67c4193483152cf9a433b07771e43d00dcb08161
sara reed <sara.reed@previdence.com>
2021-09-01 13:38:15 -0600

    CORPORATE

--- src/Components/Corperations/Corporations.js
+++ src/Components/Corperations/Corporations.js
@@ -1,6 +1,6 @@
 import React from "react";
-import corporations1 from "../../images/corporations1.jpg";
-import corporations2 from "../../images/corporations2.jpg";
+import officePeople from "../../images/officePeople.jpg";
+import highFive from "../../images/highFive.jpg";
 import NavBarTwo from "../NavBarTwo";
 import CorCarousel from "../Correctional/CorCarousel"
 
@@ -12,22 +12,24 @@ const Corporations = () => {
       <div className="corp-outer">
         <h1>Corporations</h1>
         <div className="corp-inner">
-          <img alt="office people" className="corp-img" src={corporations1} />
-          <p className="corp-p"> 
-            Previdence helps your organization create a stronger, healthier, and
-            more productive workforce. Our incomparable mental health
-            assessments inform key leaders about the strengths and risks of
-            their organization, while keeping the individuals’ information
-            confidential. Additionally, the Previdence team provides customized
-            mental health and wellness training, professional clinical support,
-            and crisis response.
-          </p>
-          <img alt="office people" className="corp-img" src={corporations2} />
+          <img alt="office people" className="corp-img image-scale box-shadow-slow" src={officePeople} />
+          <div className="corp-paragraph" >
+            <p>
+              Previdence helps your organization create a stronger, healthier, and
+              more productive workforce. Our incomparable mental health
+              assessments inform key leaders about the strengths and risks of
+              their organization, while keeping the individuals’ information
+              confidential. Additionally, the Previdence team provides customized
+              mental health and wellness training, professional clinical support,
+              and crisis response.
+            </p>
+          </div>
+          <img alt="office people" className="corp-img box-shadow-slow" src={highFive} />
+          </div>
           <div className="button-parent-corp">
             <button className="liaison">
               CONTACT OUR CORPORATIONS LIAISON
             </button>
-          </div>
         </div>
       </div>
         <CorCarousel />
