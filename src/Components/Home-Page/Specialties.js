12f523c7d07cdbc47aec2f9a1d33a1daf0df732a
Sara Reed <reedsara90@gmail.com>
2021-09-07 11:00:26 -0600

    hover transition specialties

--- src/Components/Home-Page/Specialties.js
+++ src/Components/Home-Page/Specialties.js
@@ -20,50 +20,66 @@ const Specialties = () => {
       <div className="specialties-inner">
         <div className="specialty-parent">
           <div className="specialty-item">
+          <div className="specialty-img-container">
             <img
               alt="public safety"
               className="specialty-img"
               src={publicSafety}
             />
+            </div>
             <h3 className="specialty">Public Safety</h3>
           </div>
 
           <div className="specialty-item">
+          <div className="specialty-img-container">
             <img alt="K-12" className="specialty-img" src={k12} />
+            </div>
             <h3 className="specialty">K-12</h3>
           </div>
 
           <div className="specialty-item">
+          <div className="specialty-img-container">
             <img alt="flag" className="specialty-img" src={flag} />
+            </div>
             <h3 className="specialty">Military</h3>
           </div>
 
           <div className="specialty-item">
+          <div className="specialty-img-container">
             <img alt="lady" className="specialty-img" src={profesh} />
+            </div>
             <h3 className="specialty">Professional</h3>
           </div>
 
           <div className="specialty-item">
+          <div className="specialty-img-container">
             <img alt="clouds" className="specialty-img" src={faithBased} />
+            </div>
             <h3 className="specialty">Faith Based</h3>
           </div>
 
           <div className="specialty-item">
-            <img alt="hands" className="specialty-img" src={humanitarian} />
+            <div className="specialty-img-container">
+              <img alt="hands" className="specialty-img" src={humanitarian} />
+            </div>
             <h3 className="specialty">Humanitarian</h3>
           </div>
 
           <div className="specialty-item">
+          <div className="specialty-img-container">
             <img
               alt="barbed wire with a leaf"
               className="specialty-img"
               src={correctional}
             />
+            </div>
             <h3 className="specialty">Correctional Facilities</h3>
           </div>
 
           <div className="specialty-item">
+          <div className="specialty-img-container">
             <img alt="wrist watch" className="specialty-img" src={corporate} />
+            </div>
             <h3 className="specialty">Corporations</h3>
           </div>
         </div>
