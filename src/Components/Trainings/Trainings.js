654b5d3da04d9215358f47031b7387b996fc6ee3
sara reed <sara.reed@previdence.com>
2021-09-13 09:08:34 -0600

    trainings

--- src/Components/Trainings/Trainings.js
+++ src/Components/Trainings/Trainings.js
@@ -6,14 +6,22 @@ const Trainings = () => {
 
   var client = ShopifyBuy.buildClient({
     domain: 'w4ps.myshopify.com',
-    storefrontAccessToken: 'ae653211e91e6494294f4adb2b4ca2db', // previously apiKey, now deprecated
+    storefrontAccessToken: 'd0c010618792074ac1e0a2a83a334522', // previously apiKey, now deprecated
   });
 
   var ui = ShopifyBuy.UI.init(client);
 
   ui.createComponent('product', {
     id: 6162478989468,
-    node: document.getElementById('my-product')
+    // node: document.getElementById('my-product'),
+    options: {
+      product: {
+      layout: 'horizontal',
+      contents: {
+        desciption: true
+      }
+    }
+  }
   });
 
   return (
