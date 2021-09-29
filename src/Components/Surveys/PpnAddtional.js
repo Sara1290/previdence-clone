9b2522c1fb708ea3d7ba0dfe33ff478a2484f199
sara reed <sara.reed@previdence.com>
2021-09-29 16:00:57 -0600

    ppn addtional changes

--- src/Components/Surveys/PpnAddtional.js
+++ src/Components/Surveys/PpnAddtional.js
@@ -7,23 +7,24 @@ import "survey-react/modern.css";
 const PpnAdditional = () => {
   Survey.StylesManager.applyTheme("modern");
   
-  const json2 = {
+  const json = {
     surveyId: 'a0a00c69-9355-4166-b5ef-448754bf042d',
     surveyPostId: '844fcb83-3160-4b1a-a195-2732df523f2a'
   }
-  const survey2 = new Survey.Model(json2);
+  const survey = new Survey.Model(json);
   
-  survey2.onComplete.add(function (sender) { 
+  survey.onComplete.add(function (sender) { 
     document.textContent = "Result JSON:\n" + JSON.stringify(sender.data, null, 3);
-  
-  }
-  );
+  });
+
+
+
   return (
     <div className="survey">
     <NavBarTwo />
     <h1>Previdence Provider Network - Additional Information</h1>
     <Survey.Survey
-        model={survey2}
+        model={survey}
         />
     </div>
 );
