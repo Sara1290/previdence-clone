9f9990c41bffa49c16758d927692307f1dd22a0a
sara reed <sara.reed@previdence.com>
2021-11-29 08:42:33 -0700

    new trainings

--- src/Components/Trainings/Trainings.js
+++ src/Components/Trainings/Trainings.js
@@ -7,53 +7,105 @@ import ReactGA from 'react-ga';
 
 const Trainings = () => {
   ReactGA.pageview('src/Components/Trainings/Trainings.js');
-
-
+  
+  
   var client = ShopifyBuy.buildClient({
     domain: 'w4ps.myshopify.com',
     storefrontAccessToken: 'd0c010618792074ac1e0a2a83a334522', // previously apiKey, now deprecated
   });
-
+  
   var ui = ShopifyBuy.UI.init(client);
   
+  
   useEffect(() => {
+    //Feb 1 & 2 
     ui.createComponent('product', {
-      id: 6162478989468,
+      id: 7158566125724,
       node: document.getElementById('my-product'),
       options: {
         product: {
           buttonDestination: 'modal',
           // layout: 'horizontal',
-     contents: {
-       descrition: true
+          contents: {
+            descrition: true
+          }
+        },
+        cart: {
+          startOpen: false,
+          popup: true,
+          contents: {
+            note: true,
+          },
+          text: {
+            noteDescription: 'NAME AND EMAIL OF EACH ATTENDEE'
+          }
+        }
       }
-    },
-    cart: {
-      startOpen: false,
-      popup: true,
-      contents: {
-        note: true,
-      },
-      text: {
-        noteDescription: 'NAME AND EMAIL OF EACH ATTENDEE'
-      }
-    }
-    }
     });
 
-   ui.createComponent('product', {
-     id: 7023956328604,
-     node: document.getElementById('my-product'),
-     options: {
-      product: {
-        buttonDestination: 'modal',
-      // layout: 'horizontal',
+    //Dec 16 & 17
+      ui.createComponent('product', {
+        id: 6162478989468,
+        node: document.getElementById('my-product'),
+        options: {
+          product: {
+            buttonDestination: 'modal',
+            // layout: 'horizontal',
       contents: {
         descrition: true
+        }
+      },
+      cart: {
+        startOpen: false,
+        popup: true,
+        contents: {
+          note: true,
+        },
+        text: {
+          noteDescription: 'NAME AND EMAIL OF EACH ATTENDEE'
+        }
       }
-    },
-    cart: {
-      startOpen: false,
+      }
+      });
+      //Jan 12 & 13
+      ui.createComponent('product', {
+        id: 7023956328604,
+        node: document.getElementById('my-product'),
+        options: {
+          product: {
+            buttonDestination: 'modal',
+            // layout: 'horizontal',
+            contents: {
+              descrition: true
+            }
+          },
+          cart: {
+            startOpen: false,
+            popup: true,
+            contents: {
+              note: true,
+            },
+            text: {
+              noteDescription: 'NAME AND EMAIL OF EACH ATTENDEE'
+            }
+          }
+        }
+      });
+      
+      //Jan 25 & 26
+    ui.createComponent('product', {
+      id: 7158576021660,
+      node: document.getElementById('my-product'),
+      options: {
+        product: {
+          buttonDestination: 'modal',
+          // layout: 'horizontal',
+          contents: {
+            descrition: true
+          }
+        },
+        cart: {
+          startOpen: false,
       popup: true,
       contents: {
         note: true,
@@ -63,14 +115,12 @@ const Trainings = () => {
       }
     }
   }
-});
-
+  });
 
 }, [ui])
 
-
-  return (
-    <div className="trainings-outer">
+return (
+  <div className="trainings-outer">
       <NavBar />
       <div className="trainings-inner">
         <h1>Courses</h1>
