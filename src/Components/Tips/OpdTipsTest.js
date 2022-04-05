68ad0ca149b84b42ede6700d58c7eaca118e35ce
sara reed <sara.reed@previdence.com>
2022-04-05 12:10:53 -0600

    paralax tips

--- src/Components/Tips/OpdTipsTest.js
+++ src/Components/Tips/OpdTipsTest.js
@@ -7,7 +7,7 @@ import ReactGA from 'react-ga';
 import LiaisonFooter from '../LiaisonFooter';
 
 const OpdTipsTest = () => {
-  ReactGA.pageview('src/Components/Tips/opdTips.js');
+  ReactGA.pageview('src/Components/Tips/opdTipsTest.js');
 
 
   return (
@@ -15,40 +15,47 @@ const OpdTipsTest = () => {
       <NavbarTwo />
       <div className="tips-container">
         <div className='tips-header red-dec-border background'>
-          <h3>If you are in crisis please reach out to the crisis outreach hotline</h3>
-          <h3><a className='red-link' href='tel:+1-801-273-8255'>800-273-TALK(8255)</a></h3>
-          <h3>Or contact SafeUT by using the app</h3>
-          <a href='https://safeut.org/frontline' target="_blank" rel="noreferrer">
-        <img alt="safeUT" src={safeUT} className="safeUt"/>
-        </a>
-        <h3>or calling </h3>
-        <h3 ><a className='red-link' href='tel:+1-833-372-3335'>833-372-3335</a></h3>
+            <h3>If you are in crisis please reach out to the crisis outreach hotline</h3>
+            <h3><a className='red-link' href='tel:+1-801-273-8255'>800-273-TALK(8255)</a></h3>
+            <h3>Or contact SafeUT by using the app</h3>
+            <a href='https://safeut.org/frontline' target="_blank" rel="noreferrer">
+            <img alt="safeUT" src={safeUT} className="safeUt"/>
+            </a>
+            <h3>or calling </h3>
+            <h3 ><a className='red-link' href='tel:+1-833-372-3335'>833-372-3335</a></h3>
         </div>
 
-        <div className='tips-header2 red-dec-border background'>
-            <h4>A Message From Bob</h4>
-            <p style={{fontWeight: 400, padding: 20}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
-        </div>
+
 
         <div className="CIT">
-        <img src={OPDTips} alt="odgen city police dept logo" />
-        <h1>Crisis Intervention Tips</h1>
-        <h5>Keep it simple. What do they need?</h5>
-         <p> - Safe space, new environment</p>
-         <p> - Water, snack, blanket</p>
-         <p> - Walk, if they need to</p>
-         <p> - Be present, listen, and validate</p>
-         <p> - Focus on right now</p>
-          <h5>ASK</h5>
-          <p>- What's going on?</p>
-          <p>- What triggered this?</p>
-          <p>- When was the last time you ate and slept?</p>
-          <p>- Directly ask about suicidal or homicidal intent. </p>
-              - if yes, get them to the hospital. If immediate danger, get police help.
-          <p>- What has helped you calm down in the past?</p>
-          <p>- Are you alone? Is there anyone you can be with?</p>
-          <p>- Complete Crisis Response Plan</p>
-          <p>- What is their plan for the next 24-48 hours?</p>
+            <img src={OPDTips} alt="odgen city police dept logo" />
+            <h1>Crisis Intervention Tips</h1>
+            
+            <div className='tips-header2 red-dec-borde background'>
+                <h4>A Message From Bob</h4>
+                <p style={{fontWeight: 400, padding: 20}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
+            </div>
+
+            <div className='tips-header3 red-dec-borde background'>
+                <h4>Mental Health Message</h4>
+                <p style={{fontWeight: 400, padding: 20}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
+            </div>
+            <h5>Keep it simple. What do they need?</h5>
+            <p> - Safe space, new environment</p>
+            <p> - Water, snack, blanket</p>
+            <p> - Walk, if they need to</p>
+            <p> - Be present, listen, and validate</p>
+            <p> - Focus on right now</p>
+              <h5>ASK</h5>
+              <p>- What's going on?</p>
+              <p>- What triggered this?</p>
+              <p>- When was the last time you ate and slept?</p>
+              <p>- Directly ask about suicidal or homicidal intent. </p>
+                  - if yes, get them to the hospital. If immediate danger, get police help.
+              <p>- What has helped you calm down in the past?</p>
+              <p>- Are you alone? Is there anyone you can be with?</p>
+              <p>- Complete Crisis Response Plan</p>
+              <p>- What is their plan for the next 24-48 hours?</p>
 
         </div>
         <div className="CRP">  
@@ -58,7 +65,7 @@ const OpdTipsTest = () => {
           <li>Distraction and Coping Skills</li>
           <li>Reason for Living, Motivation to Keep Thinking Forward</li>
           <li>Social Support</li>
-          <li>Professional Support <br></br> 800-273-TALK(8255) </li>
+          <li>Professional Support <br></br> <a className='link' href='tel:+1-800-273-8255'>800-273-TALK(8255) </a></li>
         </ol>
         </div>
         {/* <div className="phones">
