2cf932446fe3b1a7d8d89dde58a6dd2261ad45f7
sara reed <sara.reed@previdence.com>
2022-10-13 08:29:07 -0600

    training placeholder

--- src/Components/Trainings/Trainings.js
+++ src/Components/Trainings/Trainings.js
@@ -1,7 +1,7 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import NavBar from '../NavBar';
-import ShopifyBuy from '@shopify/buy-button-js';
-// import train2 from '../../images/train2.png';
+// import ShopifyBuy from '@shopify/buy-button-js';
+import train2 from '../../images/train2.png';
 import ReactGA from 'react-ga';
 import TrainingFooter from '../TrainingFooter';
 
@@ -11,52 +11,52 @@ const Trainings = () => {
   
   
   
- const client = ShopifyBuy.buildClient({
-    domain: 'previdence.myshopify.com',
-    storefrontAccessToken: '062fc7185279d50748982b7dc9790246', // previously apiKey, now deprecated
-  });
+//  const client = ShopifyBuy.buildClient({
+//     domain: 'previdence.myshopify.com',
+//     storefrontAccessToken: '062fc7185279d50748982b7dc9790246', // previously apiKey, now deprecated
+//   });
   
-  var ui = ShopifyBuy.UI.init(client);
+//   var ui = ShopifyBuy.UI.init(client);
   
-  client.checkout.create().then((checkout) => {
-    //do something with the checkout
-    console.log(checkout.id)
-  })
+//   client.checkout.create().then((checkout) => {
+//     //do something with the checkout
+//     console.log(checkout.id)
+//   })
 
-  useEffect(() => {
+//   useEffect(() => {
     
-    //Feb 1 & 2 
-    ui.createComponent('productSet', {
-      id: [7309811089585],
-      node: document.getElementById('my-product'),
-      options: {
-        product: {
-          buttonDestination: 'modal',
-          // layout: 'horizontal',
-          text: {
-            button: 'View Product',
-          },
-          contents: {
-            descrition: true,
+//     //Feb 1 & 2 
+//     ui.createComponent('productSet', {
+//       id: [7309811089585],
+//       node: document.getElementById('my-product'),
+//       options: {
+//         product: {
+//           buttonDestination: 'modal',
+//           // layout: 'horizontal',
+//           text: {
+//             button: 'View Product',
+//           },
+//           contents: {
+//             descrition: true,
 
-          }
-        },
-        cart: {
-          startOpen: false,
-          popup: true,
-          contents: {
-            note: true,
-          },
-          text: {
-            noteDescription: 'NAME AND EMAIL OF EACH ATTENDEE'
-          }
-        }
-      }
-    });
+//           }
+//         },
+//         cart: {
+//           startOpen: false,
+//           popup: true,
+//           contents: {
+//             note: true,
+//           },
+//           text: {
+//             noteDescription: 'NAME AND EMAIL OF EACH ATTENDEE'
+//           }
+//         }
+//       }
+//     });
 
    
 
-}, [ui])
+// }, [ui])
 
 
 return (
@@ -64,9 +64,9 @@ return (
       <NavBar />
       <div className="trainings-inner background">
         <h1>Previdence Peer Supporter Certification <br></br> For Public Safety Professionals</h1>
-        {/* <div>
+        <div>
           <img alt="check back soon for more dates" className="training-soon" src={train2} />
-        </div> */}
+        </div>
       <div className="courses-container" id="my-product">
       </div>
       </div>
