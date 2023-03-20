5812e755199e97911a6f1af56255a15fc35bb3d4
sara reed <sara.reed@previdence.com>
2023-03-20 13:50:48 -0600

    ppn recruit updates

--- src/Components/PPN/ProviderMain.js
+++ src/Components/PPN/ProviderMain.js
@@ -6,7 +6,7 @@ import './ppn.css'
 const ProviderMain = () => {
   return (
     <div className='provmain-container'>
-      <h1> Join our Previdence Professional Network </h1>
+      <h1> Previdence Professional Network </h1>
       <div className='prov-sidebyside'>
         <p className='prov-text'>
         As a Previdence Professional Network (PPN) provider, you have the opportunity to assist us in the great work of providing services for our client referrals. These individuals are seeking qualified therapists who focus on trauma-informed care and other specialized areas of therapy to help guide them on their path to healing and an enhanced quality of life. 
@@ -15,6 +15,9 @@ const ProviderMain = () => {
         <img className='prov-main-img' alt='therapists' src={ppnTherapist1Small} />
       <img className='prov-main-img display-none' alt='therapists' src={ppnTherapist2Small} />
       </div>
+      <p className='prov-text'>
+      Many of our clients are first responders and medical professionals. We're searching for therapists who are trained in highly effective evidence-based modalities, preferably EMDR and/or ART (Accelerated Resolution Therapy). Additional specialties are also welcomed.  
+        </p>
     </div>
       {/* <div className='prov-sidebyside'>
       <p className='prov-text'>who need qualified, specialized therapists to assist in guiding them on their path to healing and enhanced quality of life.
