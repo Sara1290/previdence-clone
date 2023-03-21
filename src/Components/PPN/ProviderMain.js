114de91e2273a8240abb8a577c93a11b9d0337dc
sara reed <sara.reed@previdence.com>
2023-03-21 14:16:03 -0600

    providers main content updated

--- src/Components/PPN/ProviderMain.js
+++ src/Components/PPN/ProviderMain.js
@@ -9,7 +9,7 @@ const ProviderMain = () => {
       <h1> Previdence Professional Network </h1>
       <div className='prov-sidebyside'>
         <p className='prov-text'>
-        As a Previdence Professional Network (PPN) provider, you have the opportunity to assist us in the great work of providing services for our client referrals. These individuals are seeking qualified therapists who focus on trauma-informed care and other specialized areas of therapy to help guide them on their path to healing and an enhanced quality of life. 
+        As a Previdence Professional Network (PPN) provider, your combined expertise and experience will enable us to serve individuals who are seeking qualified therapists, focused on trauma-informed care and other relevant therapy modalities to help guide them on their path to healing and enhanced quality of life.  
         </p>
         <div className='provmain-imgs'>
         <img className='prov-main-img' alt='therapists' src={ppnTherapist1Small} />
