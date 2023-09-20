2f5e280cf4586134c13d52f69b4ef06c8019d5a2
sara reed <sara.reed@previdence.com>
2023-09-20 13:41:18 -0600

    proper blog links

--- src/Components/Blog/BlogData.js
+++ src/Components/Blog/BlogData.js
@@ -22,7 +22,7 @@ export const blogdata = [
   {
     "id": 20,
     "title": "The Power of Play",
-    "link": "previdence.com/#/powerofplay",
+    "link": "#/powerofplay",
     "description": "Play is Never Over, We Just Press Pause. How Will You Play Today?",
     "img": soccer,
     "topic": "Physical Health play",
@@ -33,7 +33,7 @@ export const blogdata = [
   {
     "id": 19,
     "title": "Financial Freedom",
-    "link": "previdence.com/#/financialfreedom",
+    "link": "#/financialfreedom",
     "description": "Is it possible to be debt-free and financially independent?",
     "img": peacefulgirl,
     "topic": "Finances financial freedom",
@@ -44,7 +44,7 @@ export const blogdata = [
   {
     "id": 18,
     "title": "Culture of Wellness",
-    "link": "previdence.com/#/culture",
+    "link": "#/culture",
     "description": "Focusing on creating a culture of wellness in the first responder world.",
     "img": sherriff,
     "topic": "Wellness",
@@ -55,7 +55,7 @@ export const blogdata = [
   {
     "id": 17,
     "title": "Breathing",
-    "link": "previdence.com/#/breathing",
+    "link": "#/breathing",
     "description": "Did you know there is science that backs the positive effects of controlled breathing?",
     "img": handsin,
     "topic": "Breathing",
@@ -66,7 +66,7 @@ export const blogdata = [
   {
     "id": 16,
     "title": "Music",
-    "link": "previdence.com/#/music",
+    "link": "#/music",
     "description": "Listen to your favorite music to clear your mind and hit the reset button.",
     "img": specialtyGroups,
     "topic": "Music",
@@ -77,7 +77,7 @@ export const blogdata = [
   {
     "id": 15,
     "title": "Mental Health in a Positive Light",
-    "link": "previdence.com/#/positivelight",
+    "link": "#/positivelight",
     "description": "Adjusting our own thinking, and our interactions surrouding mental health.",
     "img": ppnTherapist6,
     "topic": "Positivity",
@@ -88,7 +88,7 @@ export const blogdata = [
   {
     "id": 14,
     "title": "Vicarious Trauma, Part 2",
-    "link": "previdence.com/#/vicariouspt2",
+    "link": "#/vicariouspt2",
     "description": "Traumatizing events can be directly experienced or witnessed; learned about from others.",
     "img": ppnTherapist4,
     "topic": "Vicarious Trauma",
@@ -99,7 +99,7 @@ export const blogdata = [
   {
     "id": 13,
     "title": "Vicarious Trauma, Part 1",
-    "link": "previdence.com/#/vicariouspt1",
+    "link": "#/vicariouspt1",
     "description": "Recognize the type of trauma you are exposed to.",
     "img": plantFlipped,
     "topic": "Vicarious Trauma",
@@ -110,7 +110,7 @@ export const blogdata = [
   {
     "id": 12,
     "title": "Neuroscience",
-    "link": "previdence.com/#/neuroscience",
+    "link": "#/neuroscience",
     "description": "3rd part in the neuroscience series, the importance of good sleep.",
     "img": resourceImg,
     "topic": "Neuroscience",
@@ -121,7 +121,7 @@ export const blogdata = [
   {
     "id": 11,
     "title": "Happiness",
-    "link": "previdence.com/#/happiness",
+    "link": "#/happiness",
     "description": "2nd part in the neuroscience series (taken from Harvard Health and Mayo Clinic).",
     "img": ppnTherapist3small,
     "topic": "Neuroscience happiness",
@@ -132,7 +132,7 @@ export const blogdata = [
   {
     "id": 10,
     "title": "Social Media",
-    "link": "previdence.com/#/socialmedia",
+    "link": "#/socialmedia",
     "description": "The brain and social media. 1st in a three-part series of neuroscience.",
     "img": ppnTherapist2small,
     "topic": "Neuroscience social media",
@@ -143,7 +143,7 @@ export const blogdata = [
   {
     "id": 9,
     "title": "Connection",
-    "link": "previdence.com/#/connection",
+    "link": "#/connection",
     "description": "Let this small message give you hope and shed light on your gifts and help you identify YOUR value. Let it help you stay connected.",
     "img": ppnTherapist1small,
     "topic": "Connection",
@@ -154,7 +154,7 @@ export const blogdata = [
   {
     "id": 8,
     "title": "Mental Health Awareness",
-    "link": "previdence.com/#/opentomhawareness",
+    "link": "#/opentomhawareness",
     "description": "Intentional conversations help remove the stigma.",
     "img": ppnTherapist5,
     "topic": "Openness mental health awareness",
@@ -165,7 +165,7 @@ export const blogdata = [
   {
     "id": 7,
     "title": "Resilience",
-    "link": "previdence.com/#/resilience",
+    "link": "#/resilience",
     "description": "Resilience is our inner strength to rebound from adversity.",
     "img": faithImage3Small,
     "topic": "Hope",
@@ -176,7 +176,7 @@ export const blogdata = [
   {
     "id": 6,
     "title": "Hope",
-    "link": "previdence.com/#/Hope",
+    "link": "#/Hope",
     "description": "A genuine sense of faith that life, truly is good. It's knowing things will get better.",
     "img": handsDarkened,
     "topic": "Hope",
@@ -187,7 +187,7 @@ export const blogdata = [
   {
     "id": 5,
     "title": "Trauma",
-    "link": "previdence.com/#/trauma",
+    "link": "#/trauma",
     "description": "Decades of research have shown trauma to be one of the leading causes of mental health concerns.",
     "img": specialtyGroups,
     "topic": "Trauma",
@@ -198,7 +198,7 @@ export const blogdata = [
   {
     "id": 4,
     "title": "Stigma",
-    "link": "previdence.com/#/stigma",
+    "link": "#/stigma",
     "description": "Stigma is stereotyping a person based on inaccurate information and/or beliefs.",
     "img": handsin,
     "topic": "Stigma",
@@ -209,7 +209,7 @@ export const blogdata = [
   {
     "id": 3,
     "title": "Sleep",
-    "link": "previdence.com/#/sleep",
+    "link": "#/sleep",
     "description": "It is vital to every living creature, you included.",
     "img": sleep2,
     "topic": "Sleep",
@@ -220,7 +220,7 @@ export const blogdata = [
   {
     "id": 2,
     "title": "Transference",
-    "link": "previdence.com/#/transference",
+    "link": "#/transference",
     "description": "A return of old feelings you had in a specific moment.",
     "img": transference,
     "topic": "Transference",
@@ -231,7 +231,7 @@ export const blogdata = [
   {
     "id": 1,
     "title": "Microbiome",
-    "link": "previdence.com/#/microbiome",
+    "link": "#/microbiome",
     "description": "Microbiome, you say? What's it got to do with sleep? Actually, what is it?",
     "img": microbiome,
     "topic": "Physical Health Microbiome",
@@ -242,7 +242,7 @@ export const blogdata = [
   {
     "id": 0,
     "title": "Sleep Hygiene",
-    "link": "previdence.com/#/sleephygiene",
+    "link": "#/sleephygiene",
     "description": "Tired? Exhausted? Can't Sleep? What do you know about sleep hygiene?",
     "img": sleep,
     "topic": "Sleep",
