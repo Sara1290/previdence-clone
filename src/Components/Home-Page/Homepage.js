1d1af9cefbc899b57073850c2d7359eeaee2fecb
sara reed <sara.reed@previdence.com>
2023-12-15 13:22:25 -0700

    no mhm but put bag the logo on the video on phones

--- src/Components/Home-Page/Homepage.js
+++ src/Components/Home-Page/Homepage.js
@@ -28,12 +28,12 @@ const Homepage = () => {
     <div>
       <NavBar />
       <TheClipComponent />
-     <div className="mhm-container">
-      <iframe className="mhm-signup" title="mhm" src="https://cdn.forms-content.sg-form.com/031f70a4-71d2-11ee-b1f0-f2593f7e8893"/>
-     </div>
       {/* <h1 className="compH1">Comprehensive Mental Health and Wellness Platform</h1> */}
       {/* <h4 className="compH3">Currently live with 26 Public Safety departments, covering over 1000 lives</h4> */}
       <GroupSection />
+     {/* <div className="mhm-container">
+      <iframe className="mhm-signup" title="mhm" src="https://cdn.forms-content.sg-form.com/031f70a4-71d2-11ee-b1f0-f2593f7e8893"/>
+     </div> */}
       {/* <Journey />*/}
       <Steps />
       <Hubspot />
