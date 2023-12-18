df3146d41f0fc2634572eb42a0907f2cc4a4e155
sara reed <sara.reed@previdence.com>
2023-12-18 15:38:18 -0700

    mhm sign up to home page

--- src/Components/Home-Page/Homepage.js
+++ src/Components/Home-Page/Homepage.js
@@ -31,9 +31,9 @@ const Homepage = () => {
       {/* <h1 className="compH1">Comprehensive Mental Health and Wellness Platform</h1> */}
       {/* <h4 className="compH3">Currently live with 26 Public Safety departments, covering over 1000 lives</h4> */}
       <GroupSection />
-     {/* <div className="mhm-container">
+     <div className="mhm-container">
       <iframe className="mhm-signup" title="mhm" src="https://cdn.forms-content.sg-form.com/031f70a4-71d2-11ee-b1f0-f2593f7e8893"/>
-     </div> */}
+     </div>
       {/* <Journey />*/}
       <Steps />
       <Hubspot />
