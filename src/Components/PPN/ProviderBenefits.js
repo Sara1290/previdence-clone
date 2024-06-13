7265182b5469fd6ab781873b7b72d99af92e730a
sara reed <sara.reed@previdence.com>
2024-06-13 15:15:05 -0600

    provider page updated

--- src/Components/PPN/ProviderBenefits.js
+++ src/Components/PPN/ProviderBenefits.js
@@ -8,7 +8,6 @@ const ProviderBenefits = () => {
       <ul className="benefits-list">
         <li>Evidence-based assessment results sent to you with ROI approval from client upon acceptance of services</li>
         <li>Individuals accepting services become your clients upon referral from Previdence</li>
-        <li>No client progress reporting to Previdence required</li>
         <li>Access to Previdence First Responder Culture Training and Previdence Assessment Training </li>
         <li>Ongoing support for therapists from the Previdence Therapist Advocate Team</li>
       </ul>
